1f6b48ebc34b9fb07b17d3a863ed7827f518457f
lttb <kenzhaev.artur@gmail.com>
2019-03-12 02:16:10 +0300

    add some optimizations for the bundles

--- docz.config.js
+++ docz.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const {css} = require('docz-plugin-css');
 
 module.exports = {
@@ -5,6 +6,7 @@ module.exports = {
     description: 'reshadow documentation',
     dest: '/dist',
     hashRouter: true,
+    // debug: true,
     repository: 'https://github.com/lttb/reshadow',
     editBranch: 'feature/initial',
     themeConfig: {
@@ -41,4 +43,35 @@ module.exports = {
             preprocessor: 'postcss',
         }),
     ],
+    modifyBundlerConfig(config, dev) {
+        if (!dev) {
+            const utilsIndex = config.entry.app.findIndex(x =>
+                x.endsWith('react-dev-utils/webpackHotDevClient.js'),
+            );
+            config.entry.app.splice(utilsIndex, 1);
+
+            config.optimization.splitChunks = {chunks: 'all'};
+            config.plugins.push(
+                new webpack.IgnorePlugin({resourceRegExp: /react-dev-utils/}),
+                new webpack.IgnorePlugin({resourceRegExp: /sockjs-client/}),
+                new webpack.IgnorePlugin({
+                    resourceRegExp: /react-error-overlay/,
+                }),
+                // new webpack.NormalModuleReplacementPlugin(
+                //     /^codemirror/,
+                //     require.resolve('./configs/stub.js'),
+                // ),
+                // new webpack.NormalModuleReplacementPlugin(
+                //     /^react-codemirror2/,
+                //     require.resolve('./configs/stub.js'),
+                // ),
+                new webpack.NormalModuleReplacementPlugin(
+                    /^buble/,
+                    require.resolve('./configs/stub.js'),
+                ),
+            );
+        }
+
+        return config;
+    },
 };
